d1ba7381f05903954045c96b51f74a9b1ce8ab28
MatiasValansi <valansimatias@gmail.com>
2025-10-06 13:33:26 -0300

    Se creó la clase AppTheme para delegar la configuración básica del tema en un objeto.

---
+++ lib/core/theme/app_theme.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+final List<Color> colorOptions = [
+  Colors.blue,
+  Colors.pink,
+  Colors.green,
+  Colors.amber,
+  Colors.purple,
+  Colors.cyan,
+  Colors.teal,
+  Colors.indigo
+];
+
+class AppTheme {
+
+  final int selectedColor;
+  final bool isDarkMode;
+
+  AppTheme({
+    this.selectedColor = 0,
+    this.isDarkMode = false,
+  });
+
+  ThemeData getTheme(){
+
+    return ThemeData(
+      colorSchemeSeed: colorOptions[this.selectedColor],
+      brightness: this.isDarkMode ? Brightness.dark : Brightness.light,
+    );    
+  }
+}
